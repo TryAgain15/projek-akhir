568e23db8b2ea0cb0083b0ce1ff7e7d6a4b602fb
TryAgain15 <hidayatramadhani5@gmail.com>
2022-12-05 11:15:45 +0700

    navbar

---
+++ navbar.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=s, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <head>
+        <div class="image">
+            
+        </div>
+    </head>
+    <nav>
+        <ul>
+            <li><a href="google.com">home</a></li> | 
+            <li><a href="fb.com">Profil</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
