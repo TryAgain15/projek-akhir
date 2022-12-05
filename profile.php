0c56050525eb531060318ab5819621b630585786
TryAgain15 <hidayatramadhani5@gmail.com>
2022-12-05 07:33:16 +0700

    profileprogres

--- profile.php
+++ profile.php
@@ -8,38 +8,58 @@
     <link rel="stylesheet" href="stylep.css">
 </head>
 <body>
+    <?php
+    include ..navbar.php
+    ?>
     <div class="konten">
         <table>
             <tr>
                 <td><img src="image/Cara-Mengunci-Foto-Profil-FB.png" alt=""></td>
                 <td style="width: 1000%;">
-                    <p>Nama : Nur Romadhoni Hidayat</p>
-                    <p>NIM : 202169040001</p>
-                    <p>Tentang :</p>
+               Tentang :
+                </td>
+            </tr>
+            <tr>
+                <td class="center mini">
+                Nur Romadhoni Hidayat
+                202169040001
+                </td>
+            </tr>
+            <tr></tr>
+            <tr>
+                <td><img src="image/Cara-Mengunci-Foto-Profil-FB.png" alt=""></td>
+                <td style="width: 1000%;">
+               Tentang :
+                </td>
+            </tr>
+            <tr>
+                <td class="center mini">
+                Rahma Diva Resita Mutiara
+                202169040024
                 </td>
             </tr>
             <tr>
                 <td><img src="image/Cara-Mengunci-Foto-Profil-FB.png" alt=""></td>
                 <td style="width: 1000%;">
-                    <p>Nama : Rahma Diva Resita Mutiara</p>
-                    <p>NIM : 202169040024</p>
-                    <p>Tentang :</p>
+               Tentang :
+                </td>
+            </tr>
+            <tr>
+                <td class="center mini">
+                Saidatul Bakdiyah
+                202169040025
                 </td>
             </tr>
             <tr>
                 <td><img src="image/Cara-Mengunci-Foto-Profil-FB.png" alt=""></td>
                 <td style="width: 1000%;">
-                    <p>Nama : Saidatul Bakdiah</p>
-                    <p>NIM : 202169040025</p>
-                    <p>Tentang :</p>
+               Tentang :
                 </td>
             </tr>
             <tr>
-            <td><img src="image/Cara-Mengunci-Foto-Profil-FB.png" alt=""></td>
-                <td style="width: 1000%;">
-                    <p>Nama : Achmad Zakiyudin</p>
-                    <p>NIM : 202169040028</p>
-                    <p>Tentang :</p>
+                <td class="center mini">
+                Achmad Zakiyudin
+                202169040028
                 </td>
             </tr>
         </table>
